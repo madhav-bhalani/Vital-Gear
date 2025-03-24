fde31365595356c7e9c62c819129f7e3d48304eb
Madhav Bhalani <madhavbhalani@gmail.com>
2025-03-24 10:19:19 +0530

    new commit monday morning

--- vital-gear/src/Components/SignIn.jsx
+++ vital-gear/src/Components/SignIn.jsx
@@ -7,6 +7,9 @@ export default function SignIn() {
   const { isSignInVisible, handleCloseModal, signInUser, setSignIn } =
     useModal();
 
+    const handleLogin = async()=>{
+      
+    }
 
   return (
     <>
